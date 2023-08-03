0b8a5c039aac50adb9f528f6901c7fec0f56b6d6
4id3n063 <141164903+4id3n063@users.noreply.github.com>
2023-08-03 12:29:04 -0500

    uh

--- Chat/Chatbot.py
+++ Chat/Chatbot.py
@@ -7,18 +7,29 @@ app = Flask(__name__)
 model = GPT2LMHeadModel.from_pretrained("facebook/opt-350m")
 tokenizer = GPT2Tokenizer.from_pretrained("facebook/opt-350m")
 
+# Initialize the conversation history
+dialogue_history = ""
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
+    global dialogue_history
+
     user_message = request.form['user_message']
 
-    # Encode the user's message and generate a response
-    input_ids = tokenizer.encode(user_message, return_tensors='pt')
+    # Append user message to the dialogue history
+    dialogue_history += user_message + " "
+
+    # Encode the dialogue history and generate a response
+    input_ids = tokenizer.encode(dialogue_history, return_tensors='pt')
     output = model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id, temperature=0.7)
     bot_response = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    # Append bot response to the dialogue history
+    dialogue_history += bot_response + " "
 
     return jsonify({'bot_response': bot_response})
 
