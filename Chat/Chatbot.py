1de8b5546f6d6abf830048457392f17072b12776
4id3n063 <141164903+4id3n063@users.noreply.github.com>
2023-08-03 12:24:27 -0500

    uh

--- Chat/Chatbot.py
+++ Chat/Chatbot.py
@@ -17,7 +17,7 @@ def send_message():
 
     # Encode the user's message and generate a response
     input_ids = tokenizer.encode(user_message, return_tensors='pt')
-    output = model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+    output = model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id, temperature=0.7)
     bot_response = tokenizer.decode(output[0], skip_special_tokens=True)
 
     return jsonify({'bot_response': bot_response})
