b8f1af2b4020de0923af5a800282dcd545be4909
4id3n063 <141164903+4id3n063@users.noreply.github.com>
2023-08-03 12:17:25 -0500

    uh

--- Chat/Chatbot.py
+++ Chat/Chatbot.py
@@ -1,6 +1,11 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
+# Load the model and tokenizer
+model = GPT2LMHeadModel.from_pretrained("facebook/opt-350m")
+tokenizer = GPT2Tokenizer.from_pretrained("facebook/opt-350m")
 
 @app.route('/')
 def home():
@@ -9,10 +14,13 @@ def home():
 @app.route('/send_message', methods=['POST'])
 def send_message():
     user_message = request.form['user_message']
-    # Add chatbot logic here to generate a response based on user_message
-    # For this example, we'll just echo the user's message as the bot's response
-    bot_response = user_message
+
+    # Encode the user's message and generate a response
+    input_ids = tokenizer.encode(user_message, return_tensors='pt')
+    output = model.generate(input_ids, max_length=100, num_return_sequences=1)
+    bot_response = tokenizer.decode(output[0], skip_special_tokens=True)
+
     return jsonify({'bot_response': bot_response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
